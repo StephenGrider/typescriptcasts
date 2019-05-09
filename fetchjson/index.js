52389f73f01c31c6fe6daed3b974ff5b40dda39b
stephen grider <ste.grider@gmail.com>
2019-05-09 11:35:42 -0600

    Executing

---
+++ fetchjson/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var axios_1 = require("axios");
+var url = 'https://jsonplaceholder.typicode.com/todos/1';
+axios_1["default"].get(url).then(function (response) {
+    console.log(response.data);
+});
---
+++ fetchjson/index.ts
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+axios.get(url).then(response => {
+  console.log(response.data);
+});
