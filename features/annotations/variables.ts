2de6ed2627a0c94abe677b26fbba85205ec6d2a0
stephen grider <ste.grider@gmail.com>
2019-05-12 14:45:48 -0600

    annotations

---
+++ features/annotations/variables.ts
@@ -0,0 +1,9 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
