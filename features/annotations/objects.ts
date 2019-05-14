f22d218246b47e761f5730322c26c65179c28f30
stephen grider <ste.grider@gmail.com>
2019-05-14 10:29:36 -0600

    annotations

---
+++ features/annotations/objects.ts
@@ -0,0 +1,16 @@
+const profile = {
+  name: 'alex',
+  age: 20,
+  coords: {
+    lat: 0,
+    lng: 15
+  },
+  setAge(age: number): void {
+    this.age = age;
+  }
+};
+
+const { age, name }: { age: number; name: string } = profile;
+const {
+  coords: { lat, lng }
+}: { coords: { lat: number; lng: number } } = profile;
--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,4 +1,4 @@
-let apples: number = 5;
+let apples = 5;
 let speed: string = 'fast';
 let hasName: boolean = true;
 
@@ -7,3 +7,50 @@ let nothing: undefined = undefined;
 
 // built in objects
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20};
+
+// 2) When we declare a variable on one line
+// and initalizate it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
