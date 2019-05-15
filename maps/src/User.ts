1c7c47b65d8487694b598b1572d6e1c32b440d11
stephen grider <ste.grider@gmail.com>
2019-05-15 15:27:48 -0600

    export

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,6 +1,6 @@
 import faker from 'faker';
 
-class User {
+export class User {
   name: string;
   location: {
     lat: number;
@@ -9,7 +9,9 @@ class User {
 
   constructor() {
     this.name = faker.name.firstName();
-    this.location.lat = parseFloat(faker.address.latitude());
-    this.location.lng = parseFloat(faker.address.longitude());
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
   }
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,5 @@
-console.log('hi there!');
+import { User } from './User';
+
+const user = new User();
+
+console.log(user);
