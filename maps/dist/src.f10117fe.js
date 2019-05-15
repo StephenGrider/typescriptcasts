74574df3055e6b5166985d405b937252883ae50f
stephen grider <ste.grider@gmail.com>
2019-05-15 15:07:46 -0600

    type coercion

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63013" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52014" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,1 +1,15 @@
-class User {}
+import faker from 'faker';
+
+class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location.lat = parseFloat(faker.address.latitude());
+    this.location.lng = parseFloat(faker.address.longitude());
+  }
+}
