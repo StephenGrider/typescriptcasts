99da3866f4008be38f4481bb626f7e909c104725
stephen grider <ste.grider@gmail.com>
2019-05-16 15:27:28 -0600

    scaffold

---
+++ sort/build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log(123);
+var logSomething = function () {
+    console.log('something');
+};
+logSomething();
---
+++ sort/src/index.ts
@@ -0,0 +1,7 @@
+console.log(123);
+
+const logSomething = () => {
+  console.log('something');
+};
+
+logSomething();
