2b7515d7b2e8e63f9079dd6d05ecdb01b3b316be
stephen grider <ste.grider@gmail.com>
2019-05-16 10:58:25 -0600

    add markers

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,3 +1,6 @@
+import { User } from './User';
+import { Company } from './Company';
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -10,4 +13,16 @@ export class CustomMap {
       }
     });
   }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng
+      }
+    });
+  }
+
+  addCompanyMarker(company: Company): void {}
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,5 +1,8 @@
-// import { User } from './User';
+import { User } from './User';
 // import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
-new CustomMap('map');
+const user = new User();
+const customMap = new CustomMap('map');
+
+customMap.addUserMarker(user);
