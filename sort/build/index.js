e5c01b28882c7f353d9b555126d8544f7d2df355
stephen grider <ste.grider@gmail.com>
2019-05-17 11:39:44 -0600

    type guard

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,6 +1,31 @@
 "use strict";
-console.log(123);
-var logSomething = function () {
-    console.log('something');
-};
-logSomething();
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                // All of this only works if collection is number[]
+                // If collection is an array of numbers
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                // Only going to work if collection is a string
+                // If collection is a string, do this logic instead:
+                // ~~~logic to compare and swap characters in a string
+                if (typeof this.collection === 'string') {
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
