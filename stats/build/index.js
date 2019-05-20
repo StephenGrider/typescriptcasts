6bdfaaf16a28e55ba33f6de9c29b8a1bc4f31b98
stephen grider <ste.grider@gmail.com>
2019-05-20 14:39:33 -0600

    parse csv

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,2 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var data = fs_1.default
+    .readFileSync('football.csv', {
+    encoding: 'utf-8'
+})
+    .split('\n')
+    .map(function (row) {
+    return row.split(',');
+});
+console.log(data);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,14 @@
 import fs from 'fs';
+
+const data = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8'
+  })
+  .split('\n')
+  .map(
+    (row: string): string[] => {
+      return row.split(',');
+    }
+  );
+
+console.log(data);
