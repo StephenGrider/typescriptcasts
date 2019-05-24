edd1e3982f227c30c115916235ee407c5badf030
stephen grider <ste.grider@gmail.com>
2019-05-24 14:02:22 -0600

    optional proeprties

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,5 +2,7 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
+user.set({ age: 999 });
+
 console.log(user.get('name'));
 console.log(user.get('age'));
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,6 +1,6 @@
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
 
 export class User {
@@ -8,5 +8,9 @@ export class User {
 
   get(propName: string): number | string {
     return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
   }
 }
