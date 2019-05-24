af0dbca522ebdc9420cad559a743854bf5e99553
stephen grider <ste.grider@gmail.com>
2019-05-24 17:46:23 -0600

    refactor

--- web/src/models/Sync.ts
+++ web/src/models/Sync.ts
@@ -8,13 +8,13 @@ export class Sync {
     return axios.get(`${this.rootUrl}/${id}`);
   }
 
-  save(data: UserProps): void {
+  save(data: UserProps): AxiosPromise {
     const { id } = data;
 
     if (id) {
-      axios.put(`${this.rootUrl}/${id}`, data);
+      return axios.put(`${this.rootUrl}/${id}`, data);
     } else {
-      axios.post(this.rootUrl, data);
+      return axios.post(this.rootUrl, data);
     }
   }
 }
