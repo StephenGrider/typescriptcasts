033d8888aa617a11f6f173943e8358fd78f4bc9c
stephen grider <ste.grider@gmail.com>
2019-05-24 15:53:20 -0600

    fetching

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,15 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 20 });
+const user = new User({ id: 1 });
 
-user.on('change', () => {
-  console.log('Change #1');
-});
-user.on('change', () => {
-  console.log('Change #2');
-});
-user.on('save', () => {
-  console.log('Save was triggered');
-});
+user.fetch();
 
-user.trigger('alskdjfalskjfdlksjdf');
+setTimeout(() => {
+  console.log(user);
+}, 4000);
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from 'axios';
+
 interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
@@ -34,5 +37,13 @@ export class User {
     handlers.forEach(callback => {
       callback();
     });
+  }
+
+  fetch(): void {
+    axios.get(`http://localhost:3000/users/${this.get('id')}`).then(
+      (response: AxiosResponse): void => {
+        this.set(response.data);
+      }
+    );
   }
 }
