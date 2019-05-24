372a17787dcec27dd9196df53fac95599bd9a5df
stephen grider <ste.grider@gmail.com>
2019-05-24 15:16:16 -0600

    trigger

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,7 +2,14 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.set({ age: 999 });
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('save', () => {
+  console.log('Save was triggered');
+});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.trigger('alskdjfalskjfdlksjdf');
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -3,7 +3,11 @@ interface UserProps {
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -12,5 +16,23 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach(callback => {
+      callback();
+    });
   }
 }
