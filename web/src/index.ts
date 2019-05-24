a4df36accf269845c1ca2793b4a04bd7dc894709
stephen grider <ste.grider@gmail.com>
2019-05-24 13:55:11 -0600

    retrieve

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hi there!');
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
---
+++ web/src/models/User.ts
@@ -0,0 +1,12 @@
+interface UserProps {
+  name: string;
+  age: number;
+}
+
+export class User {
+  constructor(private data: UserProps) {}
+
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+}
