046faf6439c9a67b0e2528472bee736776de459c
stephen grider <ste.grider@gmail.com>
2019-05-27 15:10:52 -0600

    type constraint

---
+++ web/src/models/Attributes.ts
@@ -0,0 +1,11 @@
+export class Attributes<T> {
+  constructor(private data: T) {}
+
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
+  }
+
+  set(update: T): void {
+    Object.assign(this.data, update);
+  }
+}
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -12,14 +12,4 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
 }
