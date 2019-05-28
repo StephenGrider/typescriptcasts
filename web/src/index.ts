a7af7cd02aff47bc56963523f556d4fef0240607
stephen grider <ste.grider@gmail.com>
2019-05-28 11:59:48 -0600

    Set with trigger

--- web/src/index.ts
+++ web/src/index.ts
@@ -5,7 +5,7 @@ const user = new User({ name: 'new record', age: 0 });
 console.log(user.get('name'));
 
 user.on('change', () => {
-  console.log('User was changed');
+  console.log('User was changed, we probably need to update some HTML');
 });
 
-user.trigger('change');
+user.set({ name: 'New name' });
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -30,4 +30,9 @@ export class User {
   get get() {
     return this.attributes.get;
   }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
 }
