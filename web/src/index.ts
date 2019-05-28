c87aed6ec039a61d8924bbec5ef8ae2465ba84d5
stephen grider <ste.grider@gmail.com>
2019-05-28 12:07:02 -0600

    Fetch

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,11 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'new record', age: 0 });
-
-console.log(user.get('name'));
+const user = new User({ id: 1 });
 
 user.on('change', () => {
-  console.log('User was changed, we probably need to update some HTML');
+  console.log(user);
 });
 
-user.set({ name: 'New name' });
+user.fetch();
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 import { Attributes } from './Attributes';
@@ -34,5 +35,19 @@ export class User {
   set(update: UserProps): void {
     this.attributes.set(update);
     this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+
+    this.sync.fetch(id).then(
+      (response: AxiosResponse): void => {
+        this.set(response.data);
+      }
+    );
   }
 }
