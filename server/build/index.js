f3ba5c4076ca459eb90d06c135c7b81ce7bc1287
stephen grider <ste.grider@gmail.com>
2019-05-30 14:35:24 -0600

    routes

--- server/build/index.js
+++ server/build/index.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('hi there');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+app.get('/', function (req, res) {
+    res.send("\n    <div>\n      <h1>Hi there!</h1>\n    </div>\n  ");
+});
+app.listen(3000, function () {
+    console.log('Listening on port 3000');
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hi there');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Hi there!</h1>
+    </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
