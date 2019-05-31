6f17957ee78a1c1f4dc66ef8a18be0a2bfd14e9c
stephen grider <ste.grider@gmail.com>
2019-05-31 12:38:37 -0600

    login

--- server/build/routes/loginRoutes.js
+++ server/build/routes/loginRoutes.js
@@ -16,3 +16,11 @@ router.post('/login', function (req, res) {
         res.send('Invalid email or password');
     }
 });
+router.get('/', function (req, res) {
+    if (req.session && req.session.loggedIn) {
+        res.send("\n      <div>\n        <div>You are logged in</div>\n        <a href=\"/logout\">Logout</a>\n      </div>\n    ");
+    }
+    else {
+        res.send("\n      <div>\n        <div>You are not logged in</div>\n        <a href=\"/login\">Login</a>\n      </div>\n    ");
+    }
+});
--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -33,4 +33,22 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
   }
 });
 
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+  }
+});
+
 export { router };
