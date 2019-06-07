fb4b3a64d1742a801cfb6bd3e13fc98acf5ec00b
stephen grider <ste.grider@gmail.com>
2019-06-07 17:07:57 -0600

    validate

--- rrts/src/reducers/index.ts
+++ rrts/src/reducers/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from 'redux';
+import { todosReducer } from './todos';
+import { Todo } from '../actions';
 
-export const reducers = combineReducers({
-  counter: () => 1
+export interface StoreState {
+  todos: Todo[];
+}
+
+export const reducers = combineReducers<StoreState>({
+  todos: todosReducer
 });
