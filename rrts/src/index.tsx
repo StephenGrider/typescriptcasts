ca00f1e1e863344fda7a6ff63aef49bb6a765da1
stephen grider <ste.grider@gmail.com>
2019-06-07 16:28:17 -0600

    Redux

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
 
-interface AppProps {
-  color?: string;
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-class App extends React.Component<AppProps> {
-  state = { counter: 0 };
-
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
---
+++ rrts/src/reducers/index.ts
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+
+export const reducers = combineReducers({
+  counter: () => 1
+});
