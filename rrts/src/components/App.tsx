7b3b1d96d2ff9bee4788fa47a868ffc0922c8602
stephen grider <ste.grider@gmail.com>
2019-06-07 17:23:26 -0600

    connecting

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
-export class App extends React.Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
   render() {
     return <div>Hi there!</div>;
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export const App = connect(
+  mapStateToProps,
+  { fetchTodos }
+)(_App);
