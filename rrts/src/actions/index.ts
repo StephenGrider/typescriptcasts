61e822eddd44620eb65f9bfb2e32e8cb4fce5035
stephen grider <ste.grider@gmail.com>
2019-06-07 16:49:12 -0600

    generic

--- rrts/src/actions/index.ts
+++ rrts/src/actions/index.ts
@@ -8,13 +8,18 @@ interface Todo {
   completed: boolean;
 }
 
+interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
     const response = await axios.get<Todo[]>(url);
 
-    dispatch({
+    dispatch<FetchTodosAction>({
       type: ActionTypes.fetchTodos,
       payload: response.data
     });
