c2e0fb3158d6bd1152643ce0d562e5a27c016466
stephen grider <ste.grider@gmail.com>
2019-06-07 16:42:21 -0600

    action type enum

---
+++ rrts/src/actions/index.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+export const fetchTodos = () => {
+  return async (dispatch: Dispatch) => {
+    const response = await axios.get<Todo[]>(url);
+
+    dispatch({
+      type: ActionTypes.fetchTodos,
+      payload: response.data
+    });
+  };
+};
