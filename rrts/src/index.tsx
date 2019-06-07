4669f93777ada39036b2eb189ffb1f24ac675d44
stephen grider <ste.grider@gmail.com>
2019-06-07 16:01:28 -0600

    State

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+interface AppProps {
+  color?: string;
+}
+
+class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>Hi there</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
