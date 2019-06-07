f3e5afbec0b387ae5644f31a4127003c4ef2eb9f
stephen grider <ste.grider@gmail.com>
2019-06-07 16:56:34 -0600

    reducer

--- rrts/src/actions/index.ts
+++ rrts/src/actions/index.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 import { Dispatch } from 'redux';
 import { ActionTypes } from './types';
 
-interface Todo {
+export interface Todo {
   id: number;
   title: string;
   completed: boolean;
 }
 
-interface FetchTodosAction {
+export interface FetchTodosAction {
   type: ActionTypes.fetchTodos;
   payload: Todo[];
 }
---
+++ rrts/src/reducers/todos.ts
@@ -0,0 +1,11 @@
+import { Todo, FetchTodosAction } from '../actions';
+import { ActionTypes } from '../actions/types';
+
+export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+  switch (action.type) {
+    case ActionTypes.fetchTodos:
+      return action.payload;
+    default:
+      return state;
+  }
+};
