f9c0878c6057194d5f335574f9cbd7e494c91ec4
stephen grider <ste.grider@gmail.com>
2019-06-10 15:02:52 -0600

    delete

--- rrts/src/actions/index.ts
+++ rrts/src/actions/index.ts
@@ -13,6 +13,11 @@ export interface FetchTodosAction {
   payload: Todo[];
 }
 
+export interface DeleteTodoAction {
+  type: ActionTypes.deleteTodo;
+  payload: number;
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
@@ -23,5 +28,12 @@ export const fetchTodos = () => {
       type: ActionTypes.fetchTodos,
       payload: response.data
     });
+  };
+};
+
+export const deleteTodo = (id: number): DeleteTodoAction => {
+  return {
+    type: ActionTypes.deleteTodo,
+    payload: id
   };
 };
--- rrts/src/actions/types.ts
+++ rrts/src/actions/types.ts
@@ -1,3 +1,4 @@
 export enum ActionTypes {
-  fetchTodos
+  fetchTodos,
+  deleteTodo
 }
