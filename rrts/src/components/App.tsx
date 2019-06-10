c87652d9f20ae44c306d2d4d78cc5ffe8d630af7
stephen grider <ste.grider@gmail.com>
2019-06-10 14:51:25 -0600

    render list

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -9,8 +9,23 @@ interface AppProps {
 }
 
 class _App extends React.Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  }
+
   render() {
-    return <div>Hi there!</div>;
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
