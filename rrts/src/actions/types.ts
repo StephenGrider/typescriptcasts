7bb6702314b02115f6ad1182bc89345361468cfb
stephen grider <ste.grider@gmail.com>
2019-06-10 15:22:41 -0600

    guards

--- rrts/src/actions/types.ts
+++ rrts/src/actions/types.ts
@@ -1,4 +1,8 @@
+import { FetchTodosAction, DeleteTodoAction } from './todos';
+
 export enum ActionTypes {
   fetchTodos,
   deleteTodo
 }
+
+export type Action = FetchTodosAction | DeleteTodoAction;
--- rrts/src/reducers/todos.ts
+++ rrts/src/reducers/todos.ts
@@ -1,9 +1,11 @@
-import { Todo, FetchTodosAction, ActionTypes } from '../actions';
+import { Todo, Action, ActionTypes } from '../actions';
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload);
     default:
       return state;
   }
